48a5122e1b9cd982f20d4c7add185d818dc99109
Ricky Fleming <raflemin@gmail.com>
2024-02-02 15:49:11 -0800

    create accession_numbers_doc function

--- sec_func.py
+++ sec_func.py
@@ -14,3 +14,17 @@ def ticker_to_cik(ticker: str, headers= HEADERS) -> str:
             cik = str(company['cik_str']).zfill(10)
             return cik
     raise ValueError(f'Ticker {ticker} not found')
+
+def accession_numbers_doc(ticker: str, want_10_k = True, header = HEADERS) -> dict:
+    """
+    Given the stock ticker, returns a dict of the associated accession number and document
+    for forms 10-K or 10-Q 
+    """
+    ticker_json = requests.get(f'https://data.sec.gov/submissions/CIK{ticker_to_cik(ticker)}.json', headers= HEADERS).json()
+    df = pd.DataFrame.from_dict(ticker_json['filings']['recent'], orient= 'columns')
+    if want_10_k:
+        ten_k = df[df['form'] == '10-K']
+        return ten_k[['accessionNumber', 'primaryDocument']].to_csv('split', index= False)
+    else:
+        ten_q = df[df['form'] == '10-Q']
+        return ten_q[['accessionNumber', 'primaryDocument']].to_csv('split', index= False)
