9de324fa266da43255e6fb4ed56813ef2cc06666
Ricky Fleming <raflemin@gmail.com>
2024-02-03 10:52:17 -0800

    build_archive_url function

--- sec_func.py
+++ sec_func.py
@@ -30,3 +30,7 @@ def accession_numbers(ticker: str, want_10_k = True, header = HEADERS) -> list:
     else:
         ten_q = df[df['form'] == '10-Q']
         return ten_q['accessionNumber'].to_list()
+
+def build_archive_url(ticker: str, header= HEADERS, accession_number: str) -> str:
+    url = f'https://www.sec.gov/Archives/edgar/data/{ticker_to_cik({ticker})}/{accession_number}'
+    return url
